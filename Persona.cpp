90b75a52f28d5a803d84b1342ab9a2a3072e1a00
unknown <ccorados4@miumg.edu.gt>
2026-02-15 09:52:15 -0600

    se agrego la clase persona

---
+++ Persona.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Persona{
+	//Atributos
+	protected :
+		string nombres,apellidos,direccion,fecha_nacimiento;
+		int telefono;
+		
+		//Constructor
+		Persona(){
+		}
+		Persona(string nom,string ape,string dir,int tel,string fn){
+			nombres = nom;
+			apellidos = ape;
+			direccion = dir;
+			telefono = tel;
+			fecha_nacimiento = fn;
+		}
+		// Metodos (CRUD)
+		void crear();
+		void leer();
+		void actualizar();
+		void borrar();
+		
+};
